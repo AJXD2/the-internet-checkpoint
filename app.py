14e452efec663f9ddf514b4c595b2cf8b3bf0fd2
AJXD2 <anthonyjohnkovach@gmail.com>
2023-10-05 17:55:00 -0400

    added DB functionality

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, request, redirect, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+from sqlalchemy.sql import func
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotenv()
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__, static_folder="static")
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "instance", "dev.db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 
 @app.route("/")
@@ -8,5 +36,20 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        pass
+
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
+    db.init_app(app)
+
     app.run()
---
+++ setup_env.py
@@ -0,0 +1,29 @@
+# --------------LIB--------------
+import os
+
+print("CHECKING LIBRARIES")
+try:
+    import flask, flask_sqlalchemy
+except ImportError:
+    print(
+        "CANNOT FIND LIBRARIES. INSTALLING NOW USING `current enviroment (too lazy to add this)` PACKAGE MANAGER"
+    )
+    os.system("pip install Flask Flask-SQLAlchemy")
+print("LIB CHECK DONE!")
+
+# --------------DB--------------
+from app import db, User, app
+
+print("CREATING DB")
+os.mkdir("instance")
+# Create DB/Clear previous records
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+# --------------ADMIN USER SETUP--------------
+print("Setting up admin user. (username=admin, password=admin)")
+with app.app_context():
+    admin = User(username="admin", password="admin")
+    db.session.add(admin)
+    db.session.commit()
